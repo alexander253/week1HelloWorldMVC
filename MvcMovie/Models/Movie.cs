c4e7444179c9fcde535084f622f289fe666b8734
unknown <alex_s253@hotmail.com>
2020-07-29 10:14:25 +0930

    fixed step 9 & 10

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,5 +1,11 @@
-﻿public class Movie
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
 {
+    public class Movie
+    {
     public int Id { get; set; }
 
     [StringLength(60, MinimumLength = 3)]
@@ -22,6 +28,8 @@
 
     [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
     [StringLength(5)]
-    [Required]
+
     public string Rating { get; set; }
+
+    }
 }
